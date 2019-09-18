483579cb9b1bab334d776a42dfbe800dfb4ccee6
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2019-09-18 13:29:02 -0600

    working cards commit

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Movie = (props) => {
-  const [movie, setMovie] = useState({});
+  const [movie, setMovie] = useState();
  
   useEffect(() => {
-    const id = 1;
+    const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
-
+    
        axios
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
@@ -18,7 +18,7 @@ const Movie = (props) => {
           console.error(error);
         });
 
-  },[]);
+  }, [props.match.params.id]);
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
@@ -31,7 +31,7 @@ const Movie = (props) => {
   }
 
   const { title, director, metascore, stars } = movie;
-  return (
+    return (
     <div className="save-wrapper">
       <div className="movie-card">
         <h2>{title}</h2>
